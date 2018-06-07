fc4ce1c93da775fd64e3a7854f9895461b5a165c
Jeremi RAYMOND <jraymond@e3r5p17.42.fr>
2018-06-07 21:45:04 +0200

    ss

--- path_permi.c
+++ path_permi.c
@@ -6,19 +6,47 @@
 /*   By: jraymond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 20:07:23 by jraymond          #+#    #+#             */
-/*   Updated: 2018/06/07 20:38:09 by jraymond         ###   ########.fr       */
+/*   Updated: 2018/06/07 21:44:43 by jraymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int		if_permi(char *path)
+{
+	struct stat	inf;
+
+	if (!*path)
+		return (0);
+	if (lstat(path, &inf) == -1)
+		return (-1);
+	if (!(inf.st_mode & S_IXOTH))
+		return (-1);
+	return (0);
+}
+
 int		check_permi(char *path, int len)
 {
 	char	buf[len];
+	int		x;
 
 	buf[--len] = '\0';
+	x = 0;
 	ft_memcpy(buf, path, len);
-	ft_printf("path -> %s\n", buf);
+	while (buf[x])
+	{
+		if (buf[x] == '/' && x != 0)
+		{
+			buf[x] = '\0';
+			if (if_permi(buf) == -1)
+				return (-1);
+			buf[x] = '/';
+			
+		}
+		x++;
+	}
+	if (if_permi(buf) == -1)
+		return (-1);
 	return (0);
 }
 
@@ -28,6 +56,9 @@ int		path_permi(char **path)
 
 	x = 0;
 	while (path[++x])
-		check_permi(path[x], (len_path(path[x]) + 1));
+	{
+		if (check_permi(path[x], (len_path(path[x]) + 1)) == -1)
+			ft_printf("PATH:{%s} not valide\n", path[x]);
+	}
 	return (0);
 }
